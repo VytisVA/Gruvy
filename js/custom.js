69da186695d2a654e362e8d0dbbe58ab1dd74bdb
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-07-18 16:26:33 +0100

    responsive layout for extra small devices

--- js/custom.js
+++ js/custom.js
@@ -20,6 +20,20 @@ $(function () {
         smartSpeed: 700,
         loop: true,
         autoPlayHoverPause: true,
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 2
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 3
+            }
+        }
     });
 
 });
